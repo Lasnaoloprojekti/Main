9106252ad6217be7f50d4ebd646153337f484992
Adam <adam.ahmethanov@metropolia.fi>
2023-12-12 09:28:44 +0200

    localhost

--- StudentServer/index.js
+++ StudentServer/index.js
@@ -18,7 +18,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5174",
     methods: ["GET", "POST"],
   },
 });
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   })
